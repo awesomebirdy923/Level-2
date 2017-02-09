67250fc3605cb5a6249b1cfe0fa2d9e99a82ecf6
awesomebirdy923 <league@WorthyMini-4.attlocal.net>
2017-02-08 18:33:02 -0800

    Hi.
    Hi.

--- Dr. Mario/src/GamePanel.java
+++ Dr. Mario/src/GamePanel.java
@@ -1,13 +1,14 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
 import java.net.URL;
 
-import javax.imageio.ImageIO;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
@@ -24,17 +25,34 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	private Shape doctorKeith;
 	int spacePressed = 0;
 	URL url = getClass().getResource("dr_keith_pink.png");
+	private Shape background;
+	private Shape border;
+	private Shape title;
+	private Shape jar;
 
 	public GamePanel(int fpsCap) {
-		doctorKeith = new Shape(700, 0, 300, 300, true);
+		playSound("theme.wav");
+		border = new Shape(700, 80, 300, 300, true);
+		doctorKeith = new Shape(700, 100, 300 / 2, 300 / 2, true);
+		title = new Shape(100, 0, 1, 1, true);
+		jar = new Shape(900 / 2 / 2, 400, 1, 1, true);
 		timer = new Timer(fpsCap / 60, this);
 		// pointlessSquare = new Shape(900 / 2, 900 / 2, 100, 100);
 		manager = new ObjectManager();
+		background = new Shape(0, 0, 900, 900, true);
+		background.setImage("Background.png");
 		// pill =
 		// manager.addObject(pointlessSquare);
 		// manager.addObject(pill);
+		setBackground(Color.BLACK);
 		doctorKeith.setImage("dr_keith_pink.png");
+		border.setImage("keith_border.png");
+		title.setImage("dr_keith_title.png");
+		jar.setImage("jar.png");
+		manager.addObject(title);
+		manager.addObject(border);
 		manager.addObject(doctorKeith);
+		manager.addObject(jar);
 		CURRENT_STATE = MENU_STATE;
 	}
 
@@ -59,6 +77,7 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	public void paintComponent(Graphics g) {
 		// TODO Auto-generated method stub
 		super.paintComponent(g);
+		background.renderAsImage(g);
 		if (CURRENT_STATE == MENU_STATE) {
 			paintMenuState(g);
 		} else if (CURRENT_STATE == LEVEL1_STATE) {
@@ -119,6 +138,24 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
 
+	}
+
+	public static synchronized void playSound(final String url) {
+		new Thread(new Runnable() {
+			// The wrapper thread is unnecessary, unless it blocks on the
+			// Clip finishing; see comments.
+			public void run() {
+				try {
+					Clip clip = AudioSystem.getClip();
+					AudioInputStream inputStream = AudioSystem.getAudioInputStream(getClass().getResource(url));
+					clip.open(inputStream);
+					clip.start();
+					clip.setLoopPoints(0, -1);
+				} catch (Exception e) {
+					System.err.println(e.getMessage());
+				}
+			}
+		}).start();
 	}
 
 }
